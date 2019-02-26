840986a15d9660527f717fa97dda0f038c2fd912
Josue Lopez <josue_lopez@prodigy.net>
2019-02-25 21:15:53 -0600

    Initial Commit

---
+++ Assistant Note TakerTests/Assistant_Note_TakerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Assistant_Note_TakerTests.swift
+//  Assistant Note TakerTests
+//
+//  Created by Josue Lopez on 2/25/19.
+//  Copyright © 2019 BeanProducts. All rights reserved.
+//
+
+import XCTest
+@testable import Assistant_Note_Taker
+
+class Assistant_Note_TakerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
